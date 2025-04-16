6271a1bdba34ee0275bb22de82a1cad547ad9f74
raksiarz <raksiarz@gmail.com>
2025-04-16 09:41:17 +0200

    workging tabs component

--- src/app/components/Tabs.tsx
+++ src/app/components/Tabs.tsx
@@ -3,22 +3,33 @@
 import { useState } from "react"
 
 export default function Tabs() {
-    const [itemSelected, setItemSelected] = useState(0)
-
-    const ButtonComp = () => {
-        <div className={`${itemSelected}`}>
-            <span className={`text-zinc-200 `}></span>
-        </div>
-    }
+    const [tabState, setTabState] = useState(false) 
 
     return (
-        <div className="flex gap-5 bg-indigo-100 items-center">
-            <div>
-                <span className={``}>Houses</span>
+        <div className="relative w-full mt-4 rounded-md border h-15 p-1 bg-zinc-100 text-lg">
+            <div className="relative w-full h-full flex items-center">
+                <div
+                    onClick={() => setTabState(true)}
+                    className="w-full flex justify-center text-gray-400 cursor-pointer"
+                >
+                    <button>Houses</button>
+                </div>
+                <div
+                    onClick={() => setTabState(false)}
+                    className="w-full flex justify-center text-gray-400 cursor-pointer"
+                >
+                    <button>Apartaments</button>
+                </div>
             </div>
-            <div>
-                <span>Apartament</span>
-            </div>
+
+            <span
+                className={`bg-white shadow text-lg flex items-center justify-center w-1/2 rounded h-11 transition-all duration-150 ease-linear top-2 absolute ${tabState
+                    ? 'left-1 text-indigo-600 font-semibold'
+                    : 'left-1/2 -ml-1 text-indigo-600 font-semibold'
+                    }`}
+            >
+                {tabState ? 'Houses' : 'Apartaments'}
+            </span>
         </div>
     )
 }
