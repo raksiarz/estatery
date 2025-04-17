f6a5b7524297fa60901d158f14cdea74b92c310e
raksiarz <raksiarz@gmail.com>
2025-04-17 16:19:59 +0200

    fixing issues with overflow and shadows

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -6,7 +6,6 @@ interface ContainerProps {
     tag?: boolean
     hasBorder?: boolean
     inHero?: boolean
-    shadow?: boolean
     price: string
     name: string
     description: string
@@ -14,25 +13,24 @@ interface ContainerProps {
     tabState?: boolean
 }
 
-export default function Container({ price, name, description, image, tag, inHero, shadow, hasBorder, tabState }: ContainerProps) {
+export default function Container({ price, name, description, image, tag, inHero, hasBorder, tabState }: ContainerProps) {
     return (
-        <div className={`relative flex flex-col rounded-lg bg-white w-60 h-60 lg:w-100 lg:h-100 xl:w-120 xl:h-120 border-zinc-100 hover:shadow-md transition duration-400 ease shrink-0
-            ${hasBorder && "border-1"}
-            ${shadow && "shadow-xl shadow-zinc-200"}
-            ${inHero && "scale-75 lg:scale-100 animate-fade-in"}
-        `}>
-            <div className='overflow-hidden'>
-                <Image src={image || ''} alt='house image' className='object-cover'/>
-            </div>
-            <div className='flex flex-col pl-3 pt-3 lg:pl-8 lg:pt-8 lg:pb-3 text-left lg:gap-3'>
-                <div className='flex'>
-                    <p className='text-indigo-400 font-bold text-xl lg:text-4xl mr-1'>${price}<span className='text-zinc-400 lg:text-lg text-base font-normal mt-1 lg:mt-3 ml-2'>/month</span></p>
+        <div className='relative'>
+            <div className={`overflow-hidden flex flex-col rounded-lg bg-white w-60 h-60 lg:w-100 lg:h-100 xl:w-120 xl:h-120 border-zinc-100 hover:shadow-md transition duration-400 ease shrink-0
+                ${hasBorder && "border-1"}
+                ${inHero && "scale-75 lg:scale-100 animate-fade-in shadow-xl shadow-zinc-300/30"}
+            `}>
+                <Image src={image || ''} alt='house image' className='object-cover overflow-hidden' />
+                <div className='flex flex-col pl-3 pt-3 lg:pl-8 lg:pt-8 lg:pb-3 text-left lg:gap-3'>
+                    <div className='flex'>
+                        <p className='text-indigo-400 font-bold text-xl lg:text-4xl mr-1'>${price}<span className='text-zinc-400 lg:text-lg text-base font-normal mt-1 lg:mt-3 ml-2'>/month</span></p>
+                    </div>
+                    <h3 className='text-black font-bold lg:text-3xl text-lg'>{name}</h3>
+                    <p className='text-zinc-400 lg:text-lg text-xs font-normal'>{description}</p>
                 </div>
-                <h3 className='text-black font-bold lg:text-3xl text-lg'>{name}</h3>
-                <p className='text-zinc-400 lg:text-lg text-xs font-normal'>{description}</p>
-            </div>
-            <div className='absolute bottom-[15%] right-[3%] lg:bottom-[20%] lg:right-[15%]'>
-                <LikeIcon />
+                <div className='absolute bottom-[15%] right-[3%] lg:bottom-[20%] lg:right-[15%]'>
+                    <LikeIcon />
+                </div>
             </div>
             {tag && (
                 <div className='absolute top-[52%] lg:top-[55%] left-[-6.5%] lg:left-[-3%]'>
